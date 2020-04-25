6b1a00a57e9701801dc4f22b2193b83fb3faa9fa
Matias Piano <matias@globalfishingwatch.org>
2020-04-25 20:02:31 -0300

    Removes the fleet from post_install

--- airflow/post_install.sh
+++ airflow/post_install.sh
@@ -18,7 +18,6 @@ python $AIRFLOW_HOME/utils/set_default_variables.py \
     source_naf_dataset="VMS_Chile" \
     source_naf_table="raw_naf_all_view" \
     normalized="chile_vms_normalized_" \
-    fleets=[ "aquaculture", "industry", "small_fisheries", "transport"  ] \
 
 echo "Installation Complete"
 
